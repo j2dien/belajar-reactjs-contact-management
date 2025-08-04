2ad463a8922705ddc9099429482c00c852d590c4
j2dien <129026502+j2dien@users.noreply.github.com>
2025-08-04 16:27:49 +0700

    feat: add user profile management and logout functionality

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import Layout from "./components/Layout";
 import UserRegister from "./components/User/UserRegister";
 import UserLogin from "./components/User/UserLogin";
 import DashboardLayout from "./components/DashboardLayout";
+import UserProfile from "./components/User/UserProfile";
+import UserLogout from "./components/User/UserLogout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -15,7 +17,9 @@ createRoot(document.getElementById("root")).render(
           <Route path="/login" element={<UserLogin />} />
         </Route>
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="contacts" element={<div>Contacts</div>}/>
+          <Route path="contacts" element={<div>Contacts</div>} />
+          <Route path="users/profile" element={<UserProfile />} />
+          <Route path="users/logout" element={<UserLogout />} />
         </Route>
       </Routes>
     </BrowserRouter>
